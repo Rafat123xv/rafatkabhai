f0dcfed88275ca2aded47d60b7839d52c908a1ce
Rafat123xv <124492420+Rafat123xv@users.noreply.github.com>
2023-04-02 21:56:12 +1000

    Add files via upload

--- program.cpp
+++ program.cpp
@@ -4,15 +4,15 @@
 #define STROUHAL 0.33
 
 //Calculate resultant STROUHAL air speed by reading in freq and amp
-double calculate_air_speed(double freq, double amp)
+double calculate_air_speed(int freq, int amp)
 {
-     double result;
-     result = freq * amp *0.01 / STROUHAL; //amp is converted from CM to M
-     return result;
+       double result;
+       result = freq * amp *0.01 / STROUHAL; //amp is converted from CM to M
+       return result;
 }
 
 // SIT102: CALCULATING AMP AND FREQ
-void output_air_speed(string name, double freq, double amp)
+void output_air_speed(string name, int freq, int amp)
 {
     write(name + ": ");
     write_line(calculate_air_speed(freq, amp));
@@ -28,13 +28,12 @@ string read_string(string prompt)
 }
 
 // SIT 102
-double read_integer(string prompt)
+int read_integer(string prompt)
 {
      string line;
      line = read_string(prompt);
      return convert_to_integer(line);
 }
-
 
 //SIT 102 main body
 int main()
@@ -44,18 +43,16 @@ int main()
     string student_id;
     int age;
     string bird_name;
-    double frequency;
-    double amplitude;
+    int frequency;
+    int amplitude;
 
     //get user details
     name = read_string("What is your full name: ");
     student_id = read_string("What is your student id??: ");
     age = read_integer("What is your age: ");
     bird_name = read_string("what is the name of the bird: ");
-    frequency = read_string("what is the frequency of the bird in beating its wings: ");
-    amplitude = read_string("what is the amplitude  of its stroke: ");
-
-
+    frequency = read_integer("what is the frequency of the bird in beating its wings: ");
+    amplitude = read_integer("what is the amplitude  of its stroke: ");
 
 //output to terminal
 write_line("");
@@ -72,17 +69,6 @@ write_line(frequency);
 write("amplitude: ");
 write_line(amplitude);
 write_line("");
-
-//declare variables again for the second part
-
-
-
-
-//output two
-
-write_line("");
-write_line("your age in 10 year will be: ");
-write(age + 10);
 
 //SIT102 COMMENT THWE TWO FOLLOWING LINES
 output_air_speed(bird_name,frequency,amplitude);
